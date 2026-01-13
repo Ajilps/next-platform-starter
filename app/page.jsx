b6bf46acb158855d11556153deca6a1a20a83d6c
ajilps <ajilps79@gmail.com>
2026-01-13 19:26:09 +0530

    hello world

--- app/page.jsx
+++ app/page.jsx
@@ -23,8 +23,7 @@ export default function Page() {
                 <ContextAlert className="mb-6" />
                 <h1 className="mb-4">Netlify Platform Starter – Next.js</h1>
                 <p className="mb-6 text-lg">
-                    Deploy the latest version of Next.js — including Turbopack, React Compiler, and the new caching APIs
-                    — on Netlify in seconds. No configuration or custom adapter required.
+                    Hello World!
                 </p>
                 <Link href="https://docs.netlify.com/frameworks/next-js/overview/" className="btn btn-lg sm:min-w-64">
                     Read the Docs
